829bf2f872943441b3cf1259a01490669246ab48
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-12 04:14:32 +0500

    watch

--- Hello.c
+++ Hello.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 int main(){
-    printf("Hello.world");
+    int second=0,minute=0,hour=0;
+
+    while(1){
+        
+        system("clear");
+        printf("%2d:%2d:%2d",hour,minute,second);
+        fflush(stdout);
+        second++;
+        if(second==60){
+            minute++;
+            second=0;
+        }
+        else if(minute==60){
+            hour++;
+            minute=0;
+        }
+        else if(hour==24){
+            second=0;
+            minute=0;
+            hour=0;
+        }
+        sleep(1);
+    }
     return 0;
 }
